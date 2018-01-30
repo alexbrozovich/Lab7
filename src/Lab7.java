1cb978c224a514803eb7d814ade686f393ccd384
alexbrozovich <alexbrozovich@gmail.com>
2018-01-30 09:20:13 -0500

    Fixed error where student #1 and index #1 were not the same

--- src/Lab7.java
+++ src/Lab7.java
@@ -16,14 +16,14 @@ public class Lab7 {
 			students = new String[] { "Alex Brozovich", "Willoughby McCormick", "Ronald Reagan", "Dave Thomas",
 					"Alvah Curtis Roebuck" };
 			favoriteFood = new String[] { "Tacos al Pastor", "Raw Peppercorns",
-					"Pizza with anchovies, sardines, and pickles", "Square Hamburgers", "Home cooking" };
+					"Pizza", "Square Hamburgers", "Home cooking" };
 			major = new String[] { "Leisure Studies", "Agriculture", "Economics", "Culinary Arts", "Sales" };
 
 			// prompt the user for the number of the student to get information about
 			int userIn = Validator.getInt(scan,
 					"Please enter the number for the student you want information about (1-5 inclusive)", 1,
 					students.length);
-			System.out.printf("Student number %d's name is %s \n", userIn, students[userIn]);
+			System.out.printf("Student number %d's name is %s \n", (userIn -1), students[(userIn -1)]);
 
 			// prompt the user for the type of information they would like about the student
 			String userStr = Validator.getString(scan,
@@ -33,10 +33,10 @@ public class Lab7 {
 			boolean inputGood = false;
 			while (inputGood == false) {
 				if (userStr.equalsIgnoreCase("food")) {
-					System.out.printf("%s's favorite food is %s \n", students[userIn], favoriteFood[userIn]);
+					System.out.printf("%s's favorite food is %s \n", students[(userIn -1 )], favoriteFood[(userIn -1)]);
 					inputGood = true;
 				} else if (userStr.equalsIgnoreCase("major")) {
-					System.out.printf("%s's major is %s \n", students[userIn], major[userIn]);
+					System.out.printf("%s's major is %s \n", students[(userIn -1)], major[(userIn -1)]);
 					inputGood = true;
 				} else {
 					System.out.println("Sorry, I didn't recognize that command. Please try again.");
